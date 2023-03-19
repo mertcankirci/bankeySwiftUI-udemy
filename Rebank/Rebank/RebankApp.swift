246cf9072cc7581f9b6c77efa6bf0fbfe6ef68ba
mertcankirci <mertkirci02@gmail.com>
2023-03-19 22:15:41 +0300

    Project created

---
+++ Rebank/Rebank/RebankApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebankApp.swift
+//  Rebank
+//
+//  Created by Mertcan Kırcı on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct RebankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
